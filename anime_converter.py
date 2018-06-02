179b76fabf6e9dbf954ad18dd81580b9da8f2c4a
Thomas Orton <torton@college.harvard.edu>
2018-06-02 17:48:03 +0100

    .

--- anime_converter.py
+++ anime_converter.py
@@ -221,3 +221,10 @@ def example():
     create_high_res_video("samples/AMG_test.mp4","samples/AMG_test_out.mp4")
     create_high_res_video("samples/LOGE1_test.mp4","samples/LOGE1_test_out.mp4")
     create_high_res_video("samples/VE_test.mp4","samples/VE_test_out.mp4")
+
+
+if __name__ == "__main__":
+    import sys
+    file_in=sys.argv[1]
+    file_out=sys.argv[2]
+    create_high_res_video(file_in,file_out)
